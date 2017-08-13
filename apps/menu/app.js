38031ad2ac44118158fd0e0a54829b4cc33a3427
Chris Roberts <chris@naxxfish.eu>
2017-08-13 16:50:21 +0100

    Menu app iteration

--- apps/menu/app.js
+++ apps/menu/app.js
@@ -1,6 +1,7 @@
 var __root = '../..';
 var __src = __root + '/src';
 var __js = __src + '/js';
+var Membership = require(__js + "/membership-system")
 
 var	express = require( 'express' ),
 	app = express();
@@ -8,7 +9,9 @@ var	express = require( 'express' ),
 app.set( 'views', __dirname + '/views' );
 
 app.get( '/:hashId', function ( req, res ) {
-	res.render( 'index' );
+	Membership.identify(req.params['hashId'], function (identity) {
+		res.render( 'index' , {identity: identity} );
+	});
 } );
 
 module.exports = function( config ) { return app; };
