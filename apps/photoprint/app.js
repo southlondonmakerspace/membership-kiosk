dca2c9d42db35dd089f9bd8cd4d9a74b0dacface
Chris Roberts <chris@naxxfish.eu>
2017-08-13 17:41:11 +0100

    Photo printing app stub

---
+++ apps/photoprint/app.js
@@ -0,0 +1,17 @@
+var __root = '../..';
+var __src = __root + '/src';
+var __js = __src + '/js';
+var Membership = require(__js + "/membership-system")
+
+var	express = require( 'express' ),
+	app = express();
+
+app.set( 'views', __dirname + '/views' );
+
+app.get( '/:hashId', function ( req, res ) {
+	Membership.identify(req.params['hashId'], function (identity) {
+		res.render( 'index' , {identity: identity, hash: req.params['hashId']} );
+	});
+} );
+
+module.exports = function( config ) { return app; };
