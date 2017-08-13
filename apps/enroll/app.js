07c808e10a73b7589a6e5a2ab122d2886403719c
Chris Roberts <chris@naxxfish.eu>
2017-08-13 16:49:59 +0100

    Enroll app iteration

--- apps/enroll/app.js
+++ apps/enroll/app.js
@@ -2,6 +2,8 @@ var __root = '../..';
 var __src = __root + '/src';
 var __js = __src + '/js';
 
+var Membership = require(__js + "/membership-system")
+
 var	express = require( 'express' ),
 	app = express();
 
@@ -10,5 +12,20 @@ app.set( 'views', __dirname + '/views' );
 app.get( '/:tagId', function ( req, res ) {
 	res.render( 'index' );
 } );
+
+app.post( '/:tagId', function ( req, res ) {
+	console.log(req.body.email)
+	// send a linking email to the right email address
+	Membership.enrollTag({tag: req.params['tagId'], email: req.body.email}, function (err, result) {
+		if (result)
+		{
+			res.render( 'enrolled' )
+		} else {
+			res.render( 'error', {error: err})
+		}
+
+	})
+});
+
 
 module.exports = function( config ) { return app; };
