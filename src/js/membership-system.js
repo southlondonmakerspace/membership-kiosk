5ced8ac77e2b5e6fc44009957165e10f8c1e9c2b
Tom Lynch <me@unknowndomain.co.uk>
2017-08-13 15:09:09 +0100

    Fake membership system api interface thingy

---
+++ src/js/membership-system.js
@@ -0,0 +1,20 @@
+var __root = '../..';
+var __config = __root + '/config';
+
+var request = require( 'request' ),
+	crypto = require( 'crypto' ),
+	config = require( __config + '/config.json' );
+
+var Membership = {
+	validate: function ( tag, callback ) {
+		return Math.round( Math.random() ) == 0 ? false : true;
+	},
+	hashCard: function ( id ) {
+		var md5 = crypto.createHash( 'md5' );
+		md5.update( config.tag_salt );
+		md5.update( id.toLowerCase() );
+		return md5.digest( 'hex' );
+	}
+};
+
+module.exports = Membership;
