f6c909234c2b08670539b9c939818fb6aa959eb7
Chris Roberts <chris@naxxfish.eu>
2017-08-14 19:01:40 +0100

    After 5 minutes of being on a page, it redirects back to the home screen

--- static/js/script.js
+++ static/js/script.js
@@ -14,7 +14,7 @@ socket.on ( 'card', function (card) {
 	var audioElement = document.createElement('audio');
 	audioElement.setAttribute('src', '/sounds/chime_bell_ding.wav');
 	audioElement.play()
-	
+
 	$('div.splash p').text('Sweet!')
 	$('div.splash').animate({'background-color': 'rgb(0,127,0);'},300,'swing',function () {
 		$('div.splash').animate({'left':'-100%','right':'-100%'},800,'swing',function () {
@@ -28,4 +28,7 @@ $('form').submit(function () {
 })
 $(function() {
 	$('body').hide().fadeIn(400);
+	setTimeout(function () {
+		window.location.href = "/"
+	},5*60*1000) // timeout after 5 minutes
 });
