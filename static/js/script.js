0f0445e28f71b779324f9187a558681ff1b0a7c1
Chris Roberts <chris@naxxfish.eu>
2017-08-16 23:28:56 +0100

    Nicer timeout mechanism

--- static/js/script.js
+++ static/js/script.js
@@ -25,9 +25,58 @@ socket.on ( 'card', function (card) {
 
 $(function() {
 	$('body').hide().fadeIn(400);
-	setTimeout(function () {
-		window.location.href = "/"
-	},5*60*1000) // timeout after 5 minutes
+
+	if (window.location.pathname != "/")
+	{
+		var totalSeconds = 25
+		var countdown = totalSeconds
+
+		function checkTimeout()
+		{
+			countdown--
+			console.log(countdown)
+			if (countdown <= 0)
+			{
+				// countdown is over! redirect home
+				window.location.href = "/"
+			} else {
+				if (countdown <= 15)
+				{ // less than 15 seconds to go
+					if (countdown == 15)
+					{
+						$('#secondsLeft').slideDown()
+					}
+					$('#secondsLeft').text(countdown + ' seconds left (touch the screen to cancel!)')
+					if (countdown < 5)
+					{
+						$('#secondsLeft').animate({'background-color': 'rgb(255,0,0);'},100,'swing', function() {
+							$('#secondsLeft').animate({'background-color': 'rgb(0,0,0);'},300,'swing')
+						})
+					}
+				} else {
+					$('#secondsLeft').text('').hide()
+				}
+			}
+		}
+		function resetCountdown() {
+			countdown = totalSeconds
+		}
+		$('body').on('mousemove', function () {
+			// reset the countdown
+			resetCountdown()
+		})
+		$('body').on('click', function () {
+			resetCountdown()
+			return true
+		})
+		$('body').on('keypress', function () {
+			resetCountdown()
+			return true
+		})
+		setInterval(checkTimeout,1000)
+
+	}
+
 	$('#enrollForm').submit(function (event) {
 		event.preventDefault();
 		$('.container').slideUp(800,function () {
